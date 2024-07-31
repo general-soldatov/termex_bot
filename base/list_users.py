e87159c2d4f69c0043c0cccdf472b88f5ff6eb10
general-soldatov <general-soldatov@mail.ru>
2024-07-31 21:23:28 +0300

    Add method`s for class ListUsers

--- base/list_users.py
+++ base/list_users.py
@@ -1,7 +1,7 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 from os import getenv
 from dotenv import load_dotenv
-
 
 class ListUsers:
     def __init__(self, dynamodb=None):
@@ -42,35 +42,66 @@ class ListUsers:
                 {
                 "AttributeName": "date",
                 "AttributeType": "S"
+                },
+                {
+                "AttributeName": "active",
+                "AttributeType": "N"
                 }
 
             ]
         )
         return table
 
-    def put_item(self, user_id, name, group, date):
+    def put_item(self, user_id, name, group, date, active=1):
         table = self.dynamodb.Table(self.table)
         response = table.put_item(
             Item = {
                     'user_id': user_id,
                     'name': name,
                     'group': group,
-                    'date': date
+                    'date': date,
+                    'active': active
             }
         )
         return response
 
-    def update(self, user_id, name, group, date):
+    def update_active(self, user_id, active):
         table = self.dynamodb.Table(self.table)
-        # response = table.put_item(
-        #     Item = {
-        #             'user_id': user_id,
-        #             'name': name,
-        #             'group': group,
-        #             'date': date
-        #     }
-        # )
-        # return response
+        response = table.update_item(
+            Key = {
+                'user_id': user_id
+            },
+            UpdateExpression = "set active = :a ",
+            ExpressionAttributeValues = {
+                ':a': active
+            },
+            ReturnValues = "UPDATED_NEW"
+        )
+        return response
+
+    def info_user(self, user_id):
+        table = self.dynamodb.Table(self.table)
+        response = table.query(
+            ProjectionExpression = 'user_id, name, group, date, active',
+            KeyConditionExpression = Key('user_id').eq(user_id)
+        )
+        return response['Items']
+
+    def all_users(self):
+        table = self.dynamodb.Table(self.table)
+        return table.scan()['Items']
+
+    def for_mailer(self, group=None):
+        table = self.dynamodb.Table(self.table)
+        scan_kwargs = {
+            'ProjectionExpression': "user_id, group, active"
+        }
+        response = table.scan(**scan_kwargs)
+        if group:
+            return [int(item['user_id']) for item in response['Items']
+                    if int(item['active']) and item['group'] == group]
+
+        return [int(item['user_id']) for item in response['Items'] if int(item['active'])]
 
     def delete_table(self):
         table = self.dynamodb.Table(self.table)
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from base.list_users import ListUsers
 
 def main():
     users = ListUsers()
-    users.put_item(user_id=23343, name='ddss', group='f-44', date='2024-23')
+    # users.create_table()
+    # users.delete_table()
+    # users.put_item(user_id=12313, name='ddss', group='f-43', date='2024-23')
+    # users.update_active(23343, 0)
+    # info = users.info_user(23343)
     print("Success")
+    print(users.for_mailer(group='f-44'))
+    # print(users.all_users())
 
 
 if __name__ == '__main__':
