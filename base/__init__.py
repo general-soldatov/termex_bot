3148be2a07b4089189dc73ec8c8743d790deafc6
general-soldatov <general-soldatov@mail.ru>
2024-08-01 16:55:00 +0300

    Add __init__ file in project

---
+++ base/__init__.py
@@ -0,0 +1,13 @@
+from base.list_users import ListUsers
+from base.uservar import UserVar
+from base.user_un import UserUn
+
+
+class user_study(ListUsers):
+    pass
+
+class user_var(UserVar):
+    pass
+
+class user_un(UserUn):
+    pass
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from base.list_users import ListUsers
-from base.uservar import UserVar
-from base.user_un import UserUn
+from base import user_un, user_var, user_study
 
 def main():
-    users = UserUn()
+    # Операции с базой данных
+    users = user_study()
     # users.create_table()
     # users.delete_table()
     # users.put_item(user_id=124, name='Dap', active=1)
     # users.update_active(user_id=124, active=1)
     # users.delete_note(123)
     # info = users.info_user(23343)
-    print("Success")
-    print(users.for_mailer())
-    # print(users.all_users())
+    # print(users.for_mailer())
+    print(users.all_users())
 
 
 if __name__ == '__main__':
