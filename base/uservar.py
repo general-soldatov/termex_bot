cd8fbdbd0666c5dfd27f8f33632c995a9d57278c
general-soldatov <general-soldatov@mail.ru>
2024-08-01 15:19:00 +0300

    Add class for control student`s tasks

---
+++ base/uservar.py
@@ -0,0 +1,83 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from os import getenv
+from dotenv import load_dotenv
+
+class UserVar:
+    def __init__(self, dynamodb=None):
+        self.dynamodb = dynamodb
+        self.table = 'User_Var'
+        if not self.dynamodb:
+            load_dotenv()
+            self.dynamodb = boto3.resource(
+                'dynamodb',
+                endpoint_url=getenv('ENDPOINT'),
+                region_name='ru-central1',
+                aws_access_key_id=getenv('AWS_ACCESS_KEY_ID'),
+                aws_secret_access_key=getenv('AWS_SECRET_ACCESS_KEY')
+                )
+
+    def create_table(self):
+        table = self.dynamodb.create_table(
+            TableName = self.table,
+            KeySchema = [
+                {
+                    'AttributeName': 'user_id',
+                    'KeyType': 'HASH'  # Ключ партицирования
+                }
+            ],
+            AttributeDefinitions = [
+                {
+                "AttributeName": "user_id",
+                "AttributeType": "N"
+                },
+                {
+                "AttributeName": "name",
+                "AttributeType": "S"
+                },
+                {
+                "AttributeName": "group",
+                "AttributeType": "S"
+                },
+
+            ]
+        )
+        return table
+
+    def put_item(self, user_id, name, group, var, var_d1, bonus=0):
+        table = self.dynamodb.Table(self.table)
+        all_var = {'var_all': var, 'var_d1': var_d1}
+        tasks = {'S1': 0}
+        response = table.put_item(
+            Item = {
+                    'user_id': user_id,
+                    'name': name,
+                    'group': group,
+                    'var': all_var,
+                    'tasks': tasks,
+                    'bonus': bonus
+            }
+        )
+        return response
+
+    def add_task(self, user_id, task, ball):
+        table = self.dynamodb.Table(self.table)
+        response = table.update_item(
+            Key = {
+                'user_id': user_id
+            },
+            UpdateExpression = f"set tasks.{task} = :t ",
+            ExpressionAttributeValues = {
+                ':t': ball
+            },
+            ReturnValues = "UPDATED_NEW"
+        )
+        return response
+
+    def all_users(self):
+        table = self.dynamodb.Table(self.table)
+        return table.scan()['Items']
+
+    def delete_table(self):
+        table = self.dynamodb.Table(self.table)
+        table.delete()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from base.list_users import ListUsers
+from base.uservar import UserVar
 
 def main():
-    users = ListUsers()
+    users = UserVar()
     # users.create_table()
     # users.delete_table()
-    # users.put_item(user_id=12313, name='ddss', group='f-43', date='2024-23')
-    # users.update_active(23343, 0)
+    # users.put_item(user_id=123, name='Dap', group='f-44', var=22, var_d1=11)
+    users.add_task(user_id=123, task='S4', ball=6)
     # info = users.info_user(23343)
     print("Success")
-    print(users.for_mailer(group='f-44'))
-    # print(users.all_users())
+    # print(users.for_mailer(group='f-44'))
+    print(users.all_users())
 
 
 if __name__ == '__main__':
